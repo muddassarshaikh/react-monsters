01482e4875671306d7a9334e1646d88d7b503e01
Muddassar S Shaikh <shaikh.muddassar8@gmail.com>
2020-05-08 15:50:33 +0530

    Updates

---
+++ precache-manifest.a2b665019d828b9ab175c7c2ca384ab6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43bb1aad88a42d1225ec25614cc9474d",
+    "url": "/react-monsters/index.html"
+  },
+  {
+    "revision": "25d6fc44b65206723593",
+    "url": "/react-monsters/static/css/main.afbf49dc.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/react-monsters/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-monsters/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "25d6fc44b65206723593",
+    "url": "/react-monsters/static/js/main.ca18b023.chunk.js"
+  },
+  {
+    "revision": "9e48f53adff59196fe9e",
+    "url": "/react-monsters/static/js/runtime-main.265b368b.js"
+  }
+]);
